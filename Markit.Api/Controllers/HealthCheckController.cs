b61eb08ae72eb3801f5432a22258655acdcc08c0
bckcmo <bcorazzin@gmail.com>
2020-04-21 19:02:35 -0500

    troubleshooting db conn

--- Markit.Api/Controllers/HealthCheckController.cs
+++ Markit.Api/Controllers/HealthCheckController.cs
@@ -1,17 +1,27 @@
-﻿using Markit.Api.Models.Dtos;
+﻿using System;
+using Markit.Api.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Markit.Api.Controllers
 {
     [ApiController, Route("healthcheck")]
     public class HealthCheckController : Controller
     {
+        private readonly IConfiguration _configuration;
+        public HealthCheckController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
         [HttpGet]
         public IActionResult Index()
         {
             return Ok(new Healthcheck
             {
-                AmIHealthy = true
+                AmIHealthy = true,
+                Message = $"Connection String: {_configuration.GetConnectionString("MySql")}, EnvVar: {Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb")}"
             });
         }
     }
--- Markit.Api/Models/Dtos/Healthcheck.cs
+++ Markit.Api/Models/Dtos/Healthcheck.cs
@@ -3,5 +3,6 @@ namespace Markit.Api.Models.Dtos
     public class Healthcheck
     {
         public bool AmIHealthy { get; set; }
+        public string Message { get; set; }
     }
 }
