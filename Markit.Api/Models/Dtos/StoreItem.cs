0265f9042db7bb215d2220b830de28b0c738abdc
bckcmo <bcorazzin@gmail.com>
2020-04-26 13:38:15 -0500

    added storeid to store item

--- Markit.Api/Models/Dtos/StoreItem.cs
+++ Markit.Api/Models/Dtos/StoreItem.cs
@@ -4,6 +4,7 @@ namespace Markit.Api.Models.Dtos
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+        public int StoreId { get; set; }
         public string Upc { get; set; }
         public decimal Price { get; set; }
         public bool IsSalePrice { get; set; }
